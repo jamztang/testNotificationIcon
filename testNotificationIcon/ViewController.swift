00c4e18b0a8316601fbd3f5e61b5804ea1921541
James Tang <james@goodnotesapp.com>
2020-05-20 11:08:43 +0800

    Add UI to control badge value

--- testNotificationIcon/ViewController.swift
+++ testNotificationIcon/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var textField: UITextField!
+    private let tapGestureRecognizer: UITapGestureRecognizer = .init()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        guard let item = self.tabBarController?.tabBarItem else { fatalError() }
+        Swift.print("item \(item)")
+        Swift.print("item \(self.tabBarItem)")
+        self.tabBarItem.badgeValue = "1"
+//        item.badgeValue = value
+
+        stepper.addTarget(self, action: #selector(handleStepper(_:)), for: .valueChanged)
+        textField.addTarget(self, action: #selector(handleTextFieldDidChange(_:)), for: .editingDidEnd)
+
+        view.addGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer.addTarget(self, action: #selector(handleTap(_:)))
     }
 
+    @IBAction func handleStepper(_ sender: Any) {
+        let value = stepper.value > 0 ? "\(Int(stepper.value))": nil
+        self.tabBarItem.badgeValue = value
+    }
 
+    @IBAction func handleTextFieldDidChange(_ sender: Any) {
+        let value = textField.text
+        self.tabBarItem.badgeValue = value
+    }
+
+    @IBAction func handleTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 }
 

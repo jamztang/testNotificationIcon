6a39b5c7a99ee3ad293b20eb8981af812f33261a
James Tang <james@goodnotesapp.com>
2020-05-20 10:48:36 +0800

    Initial Commit

---
+++ testNotificationIcon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testNotificationIcon
+//
+//  Created by James Tang on 20/5/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
